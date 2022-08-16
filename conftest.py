7fdcd8ea29305dddbb489c5f3a0c3179828f5836
Maxime Soydas <max.soydas@gmail.com>
2022-08-17 00:41:11 +0200

    Wip test login

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+import server
+
+
+@pytest.fixture
+def client():
+    with server.app.test_client() as client:
+        yield client
--- pytest/test_point_deduction.py
+++
@@ -1,5 +0,0 @@
-def test_load_clubs():
-    with open('clubs.json', 'r') as jsonfile:
-        return jsonfile.read()
-    
-print(test_load_clubs())    
