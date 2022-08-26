baafabfd65ebc48e42f5d3c7bc7d1324d7dd7149
Maxime Soydas <max.soydas@gmail.com>
2022-08-26 15:40:20 +0200

    wip

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,6 @@ from app.server import create_app
 
 @pytest.fixture
 def client():
-    app = create_app({"TESTING": True})
+    app = create_app(config={"TESTING": True})
     with app.test_client() as client:
         yield client
